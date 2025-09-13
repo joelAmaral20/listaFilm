52579999f97ce1587029beeec326537bdca04a60
joelAmaral20 <amaraljoel194@gmail.com>
2025-09-13 17:28:14 +0100

    lista-filmes

---
+++ script.js
@@ -0,0 +1,28 @@
+// criando uma lista de filmes usando
+const minhaListafilmes = [
+    filmesAcao = {
+        nome: "o reencontro",
+        anoLancamento: 2021,
+        atorPricipal: "marcos",
+        cateGoria: "Acao"
+    },
+    filmesterror = {
+        nome: "o fantasma",
+        anoLancamento: 2002,
+        atorPricipal: "helmer",
+        cateGoria: "terror"
+    },
+    filmesromance = {
+        nome: "o Amor infalivel",
+        anoLancamento: 2000,
+        atorPricipal: "carlos",
+        cateGoria: "romance"
+    },
+    filmesComedia ={
+        nome: "o jogo do riso",
+        anoLancamento: 2004,
+        atorPricipal: "Ana ool",
+        cateGoria: "comedia"
+    }
+]
+console.table(minhaListafilmes)
